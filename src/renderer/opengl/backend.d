bc29ee73012202b7b12885df5303ccbca1bd93de
Xavier Bigand <flamaros.xavier@gmail.com>
2016-12-15 23:37:39 +0100

    Add Shader module

--- src/renderer/opengl/backend.d
+++ src/renderer/opengl/backend.d
@@ -2,11 +2,13 @@ module renderer.opengl.backend;
 
 import renderer.opengl.util : checkgl;
 import renderer.opengl.geometry;
+import renderer.opengl.shader;
 
 import derelict.opengl3.gl3;
 
 import std.experimental.logger;
 import std.string;
+import std.conv;
 
 static Backend	backend;
 
@@ -17,14 +19,32 @@ struct Backend
 		DerelictGL3.load();
 		DerelictGL3.reload();
 
+		OpenGLVersion = to!string(glGetString(GL_VERSION));
+		GLSLVersion = to!string(glGetString(GL_SHADING_LANGUAGE_VERSION));
 		infof("OpenGL Vendor: \"%s\"", fromStringz(glGetString(GL_VENDOR)));
 		infof("OpenGL Renderer: \"%s\"", fromStringz(glGetString(GL_RENDERER)));
-		infof("OpenGL Version: \"%s\"", fromStringz(glGetString(GL_VERSION)));
+		infof("OpenGL Version: \"%s\"", OpenGLVersion);
 //		infof("OpenGL Extensions: \"%s\"", fromStringz(glGetString(GL_EXTENSIONS)));
-		infof("OpenGL GLSL Version: \"%s\"", fromStringz(glGetString(GL_SHADING_LANGUAGE_VERSION)));
+		infof("OpenGL GLSL Version: \"%s\"", GLSLVersion);
 
 		triangle.create();
 		triangle.set();
+
+		Shader	vertexShader;
+		Shader	fragmentShader;
+
+		vertexShader.create();
+		vertexShader.load("data/shaders/DiffuseColor.vsh", GL_VERTEX_SHADER);
+		vertexShader.compile();
+
+		fragmentShader.create();
+		fragmentShader.load("data/shaders/DiffuseColor.fsh", GL_FRAGMENT_SHADER);
+		fragmentShader.compile();
+
+		program.create();
+		program.attachShader(vertexShader);
+		program.attachShader(fragmentShader);
+		program.link();
 	}
 
 	void	draw()
@@ -33,8 +53,14 @@ struct Backend
 		triangle.draw();
 	}
 
+package:
+	// TODO clean version string to be usable in shaders,...
+	string	OpenGLVersion;
+	string	GLSLVersion;
+
 private:
-	Geometry	triangle;
+	Geometry		triangle;
+	ShaderProgram	program;
 }
 
 version (Windows)
--- src/renderer/opengl/shader.d
+++ src/renderer/opengl/shader.d
@@ -1,17 +1,112 @@
 module renderer.opengl.shader;
 
 import renderer.opengl.util : checkgl;
+import renderer.opengl.backend;
 
 import derelict.opengl3.gl3;
 
+import std.experimental.logger;
+import std.file;
+
 struct Shader
 {
-	void	load(ref const string vertexFilePath, ref const string fragmentFilePath)
+	void	create()
 	{
+		mGLSLVersion = backend.GLSLVersion;
+	}
+
+	void	load(string filePath, GLenum type)
+	{
+		mFilePath = filePath;
+		mSource = cast(string)read(filePath);
+		mType = type;
+
+		mId = checkgl!glCreateShader(mType);
+	}
+
+	void	compile()
+	{
+		GLint	length;
+
+		length = cast(GLint)mSource.length;
+
+		auto	ssp = mSource.ptr;
+		checkgl!glShaderSource(mId, 1, &ssp, &length);
+
+		checkgl!glCompileShader(mId);
+
+		GLint status;
+		checkgl!glGetShaderiv(mId, GL_COMPILE_STATUS, &status);
+
+		if (status == GL_FALSE)
+		{
+			GLint logLength;
+			checkgl!glGetShaderiv(mId, GL_INFO_LOG_LENGTH, &logLength);
+
+			if (logLength > 0)
+			{
+				ubyte[]	log;
+
+				log.length = logLength;
+				checkgl!glGetShaderInfoLog(mId, logLength, &logLength, cast(GLchar*)log.ptr);
+
+				critical(cast(string)log);
+			}
+			criticalf("Failed to compile shader: %s", mFilePath);
+		}
+	}
+
+private:
+	GLuint	mId;
+	GLenum	mType;
+
+	string	mGLSLVersion;
+	string	mPreprocessors;
+	string	mFilePath;
+	string	mSource;
+}
+
+struct ShaderProgram
+{
+	void	create()
+	{
+		mProgram = checkgl!glCreateProgram();
+	}
+
+	void	attachShader(ref const Shader shader)
+	{
+		checkgl!glAttachShader(mProgram, shader.mId);
+	}
+
+	void	link()
+	{
+		checkgl!glLinkProgram(mProgram);
+
+		GLint status;
+		checkgl!glGetProgramiv(mProgram, GL_LINK_STATUS, &status);
+		if (status == GL_FALSE)
+		{
+			debug	// Retrieve the log
+			{
+				//checkgl!glValidateProgram(mShaderProgram);
+				GLint	logLength;
+				checkgl!glGetProgramiv(mProgram, GL_INFO_LOG_LENGTH, &logLength);
+				if (logLength > 0)
+				{
+					GLchar[]	log = new char[](logLength);
+
+					glGetProgramInfoLog(mProgram, logLength, &logLength, log.ptr);
+					if (logLength > 0)	// It seems GL_INFO_LOG_LENGTH can return 1 instead of 0
+						critical("Shader log :\n" ~ log);
+				}
+			}
+			critical("Failed to link program");
+		}
 	}
 
 	void	use()
 	{
+		checkgl!glUseProgram(mProgram);
 	}
 
 private:
